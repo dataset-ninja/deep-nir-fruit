452d35ae94f5d7efb15ec199ba9ef2e297479185
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-06-13 18:37:01 +0300

    Prepared for uploading.

--- src/convert.py
+++ src/convert.py
@@ -1,15 +1,108 @@
-# Path to the original dataset
+import glob
+import os
 
 import supervisely as sly
+from supervisely.io.fs import file_exists, get_file_name, get_file_name_with_ext
 
 
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    # * dataset_path = "Path to the local dir with dataset"
+    dataset_path = "/home/alex/DATASETS/TODO/deep_fruit/yolov5"
+    # images_folder = "images"
+    anns_folder = "labels"
+    ann_ext = ".txt"
+    batch_size = 30
 
-    # Function should read local dataset and upload it to Supervisely project, then return project info.
+    def create_ann(image_path):
+        labels = []
 
-    raise NotImplementedError("The converter should be implemented manually.")
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
 
-    # * return project
+        class_name = image_path.split(dataset_path)[1].split("/")[1]
+        obj_class = name_to_class[class_name]
+
+        bbox_path = os.path.join(
+            image_path.split("images")[0], anns_folder, get_file_name(image_path) + ann_ext
+        )
+
+        if file_exists(bbox_path):
+            with open(bbox_path) as f:
+                content = f.read().split("\n")
+
+                for curr_data in content:
+                    if len(curr_data) != 0:
+                        curr_data = list(map(float, curr_data.split(" ")))
+
+                        left = int((curr_data[1] - curr_data[3] / 2) * img_wight)
+                        right = int((curr_data[1] + curr_data[3] / 2) * img_wight)
+                        top = int((curr_data[2] - curr_data[4] / 2) * img_height)
+                        bottom = int((curr_data[2] + curr_data[4] / 2) * img_height)
+                        rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+                        label = sly.Label(rectangle, obj_class)
+                        labels.append(label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+    obj_class_apple = sly.ObjClass("apple", sly.Rectangle)
+    obj_class_avocado = sly.ObjClass("avocado", sly.Rectangle)
+    obj_class_blueberry = sly.ObjClass("blueberry", sly.Rectangle)
+    obj_class_capsicum = sly.ObjClass("capsicum", sly.Rectangle)
+    obj_class_cherry = sly.ObjClass("cherry", sly.Rectangle)
+    obj_class_mango = sly.ObjClass("mango", sly.Rectangle)
+    obj_class_orange = sly.ObjClass("orange", sly.Rectangle)
+    obj_class_rockmelon = sly.ObjClass("rockmelon", sly.Rectangle)
+    obj_class_strawberry = sly.ObjClass("strawberry", sly.Rectangle)
+    obj_class_kiwi = sly.ObjClass("kiwi", sly.Rectangle)
+    obj_class_wheat = sly.ObjClass("wheat", sly.Rectangle)
+
+    name_to_class = {
+        "apple": obj_class_apple,
+        "avocado": obj_class_avocado,
+        "blueberry": obj_class_blueberry,
+        "capsicum": obj_class_capsicum,
+        "cherry": obj_class_cherry,
+        "mango": obj_class_mango,
+        "orange": obj_class_orange,
+        "rockmelon": obj_class_rockmelon,
+        "strawberry": obj_class_strawberry,
+        "kiwi": obj_class_kiwi,
+        "wheat": obj_class_wheat,
+    }
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=list(name_to_class.values()))
+    api.project.update_meta(project.id, meta.to_json())
+
+    all_train_images_pathes = glob.glob(dataset_path + "/*/train/*/*.jpg")
+    all_valid_images_pathes = glob.glob(dataset_path + "/*/valid/*/*.jpg")
+    all_test_images_pathes = glob.glob(dataset_path + "/*/test/*/*.jpg")
+
+    ds_to_pathes = {
+        "train": all_train_images_pathes,
+        "valid": all_valid_images_pathes,
+        "test": all_test_images_pathes,
+    }
+
+    for ds_name in list(ds_to_pathes.keys()):
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+        images_pathes = ds_to_pathes[ds_name]
+        # anns_path = os.path.join(dataset_path, ds_name, anns_folder)
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_pathes))
+
+        for img_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
+            img_names_batch = [get_file_name_with_ext(im_path) for im_path in img_pathes_batch]
+
+            img_infos = api.image.upload_paths(dataset.id, img_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns = [create_ann(image_path) for image_path in img_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns)
+
+            progress.iters_done_report(len(img_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -5,31 +5,33 @@ from dataset_tools.templates import AnnotationType, CVTask, Industry, License
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: Optional[str] = None
+PROJECT_NAME: str = "deepNIR: Improved fruit detection system"
+PROJECT_NAME_FULL: Optional[str] = "deepNIR: Improved fruit detection system"
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-INDUSTRIES: List[Industry] = None
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+LICENSE: License = License.CC_BY_4_0()
+INDUSTRIES: List[Industry] = [Industry.Agriculture()]
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_YEAR: int = None
-HOMEPAGE_URL: str = None
+RELEASE_YEAR: int = 2022
+HOMEPAGE_URL: str = "https://zenodo.org/record/6324489#.YkbEvX9Bzmg"
 # e.g. "https://some.com/dataset/homepage"
 
 PREVIEW_IMAGE_ID: int = None
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/deep-nir-fruit"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[
+    Union[str, dict]
+] = "https://zenodo.org/record/6324489/files/yolov5.zip?download=1"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -47,7 +49,7 @@ TAGS: List[str] = None
 
 
 def check_names():
-    fields_before_upload = [PROJECT_NAME] # PROJECT_NAME_FULL
+    fields_before_upload = [PROJECT_NAME]  # PROJECT_NAME_FULL
     if any([field is None for field in fields_before_upload]):
         raise ValueError("Please fill all fields in settings.py before uploading to instance.")
 
@@ -64,10 +66,10 @@ def get_settings():
         "preview_image_id": PREVIEW_IMAGE_ID,
         "github_url": GITHUB_URL,
     }
-    
+
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
-   
+
     settings["project_name_full"] = PROJECT_NAME_FULL
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
